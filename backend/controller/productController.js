26b88329fbabf6f0396aadfb5397cd5d60e590b0
emanFriendTales <eman@friendtales.com>
2023-10-30 12:33:18 +0500

    added crud operations for product

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -55,3 +55,20 @@ exports.getAllProducts = async (req,res)=> {
     const products = await Product.find()
     res.status(200).json({success: "true", products});
 }
+
+
+exports.getProductDetail = async (req,res)=> {
+    let product = await Product.findById(req.params.id);
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    
+    res.status(200).json({
+        success: true,
+        message: "Product found successfully",
+        product
+    })
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllProducts, createProduct, updateProduct, deleteProduct } = require('../controller/productController');
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetail } = require('../controller/productController');
 const router = express.Router();
 
 
@@ -7,6 +7,8 @@ router.route('/').get(getAllProducts);
 router.route('/create').post(createProduct);
 router.route('/update/:id').put(updateProduct);
 router.route('/delete/:id').delete(deleteProduct);
+router.route('/:id').get(getProductDetail);
+
 
 
 
