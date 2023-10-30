85f3de53a46d1504faaf8f7ab8fcfc0ce8e2b935
emanFriendTales <eman@friendtales.com>
2023-10-30 12:25:29 +0500

    added crud operations for product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getAllProducts, createProduct } = require('../controller/productController');
+const { getAllProducts, createProduct, updateProduct, deleteProduct } = require('../controller/productController');
 const router = express.Router();
 
 
 router.route('/').get(getAllProducts);
 router.route('/create').post(createProduct);
+router.route('/update/:id').put(updateProduct);
+router.route('/delete/:id').delete(deleteProduct);
+
 
 
 
