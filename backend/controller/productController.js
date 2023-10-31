d812eab118c6bc7e61d5069d2e6c4ccd276cd97f
emanFriendTales <eman@friendtales.com>
2023-10-31 22:35:11 +0500

    added async error handling

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,14 +1,15 @@
 const Product = require('../models/Product');
 const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncError = require('../middleware/asyncError');
 
 // create new product, admin
-exports.createProduct = async (req,res, next)=> {
+exports.createProduct = catchAsyncError(async (req,res, next)=> {
 
     const newProduct = await Product.create(req.body);
     res.status(201).json({message: "Product added successfully!", newProduct})
-}
+});
 
-exports.updateProduct = async(req,res, next) => {
+exports.updateProduct = catchAsyncError( async(req,res, next) => {
     let product = await Product.findById(req.params.id);
     if(!product){
         return next(new ErrorHandler(500, "Product not found"));
@@ -25,10 +26,10 @@ exports.updateProduct = async(req,res, next) => {
         message: "Product Updated Successfully",
         product
     })
-}
+});
 
 
-exports.deleteProduct = async (req,res,next) => {
+exports.deleteProduct =catchAsyncError(async (req,res,next) => {
     let product = await Product.findById(req.params.id);
     if(!product){
         return next(new ErrorHandler(500, "Product not found"));
@@ -43,16 +44,16 @@ exports.deleteProduct = async (req,res,next) => {
 
     })
 
-}
+});
 
 
-exports.getAllProducts = async (req,res)=> {
+exports.getAllProducts =catchAsyncError( async (req,res)=> {
     const products = await Product.find()
     res.status(200).json({success: "true", products});
-}
+});
 
 
-exports.getProductDetail = async (req,res)=> {
+exports.getProductDetail =catchAsyncError( async (req,res)=> {
     let product = await Product.findById(req.params.id);
     if(!product){
         return next(new ErrorHandler(500, "Product not found"));
@@ -64,4 +65,4 @@ exports.getProductDetail = async (req,res)=> {
         message: "Product found successfully",
         product
     })
-}
+});
