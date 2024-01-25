1a994d7858de45c97432d2973a622a956a7c7028
emanFriendTales <eman@friendtales.com>
2024-01-25 23:41:33 +0500

    made some changes

--- backend/middleware/auth-verify.js
+++ backend/middleware/auth-verify.js
@@ -1,9 +1,13 @@
 const jwt = require('jsonwebtoken');
-const secretKey = 'your-secret-key';
+const crypto = require('crypto');
+const secretKey = crypto.randomBytes(32).toString('hex');
 
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      throw new ErrorHandler(401, 'Token Authentication failed');
+    }
     const decodedToken = jwt.verify(token, secretKey);
     req.userData = { userId: decodedToken.userId, email: decodedToken.email };
     next();
