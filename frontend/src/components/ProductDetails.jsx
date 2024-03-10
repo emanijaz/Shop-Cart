1396c4bbf68494c546e5bfe2d69bb14c31b4a661
emanFriendTales <eman@friendtales.com>
2024-03-10 22:34:32 +0500

    improving product details page

--- frontend/src/components/ProductDetails.jsx
+++ frontend/src/components/ProductDetails.jsx
@@ -6,6 +6,8 @@ import Navbar from './Navbar';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { cartActions } from '../store/cartslice';
+import TextField from '@mui/material/TextField';
+import Alert from '@mui/material/Alert';
 
 export default function ProductDetails() {
     const { id } = useParams();
@@ -16,8 +18,24 @@ export default function ProductDetails() {
     const [reviewComment, setReviewComment] = useState('');
     const [reviewRating, setReviewRating] = useState(0);
     const [alertMessage, setAlertMessage] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+
     const [alertColor, setAlertColor] = useState('success');
     const dispatch = useDispatch();
+
+    const handleQuantityChange = (event) => {
+        const newQuantity = parseInt(event.target.value);
+        console.log('stock: ', product.stock)
+        if (newQuantity >= 1 && newQuantity <= product.stock) {
+            console.log('setting new quant')
+            setProductQuantity(newQuantity);
+            setShowAlert(false);
+        }
+        else{
+            setShowAlert(true);
+        }
+
+    };
 
     const addToCart = () => {
         dispatch(cartActions.addToCart({
@@ -85,6 +103,30 @@ export default function ProductDetails() {
         getProductDetails();
     })
 
+    const addProductQuantity = () =>{
+        console.log(product.stock)
+        if(productQuantity < product.stock){
+            setProductQuantity(productQuantity+1);
+        }
+    }
+    const decreaseProductQuantity = () =>{
+        if(productQuantity >1){
+            setProductQuantity(productQuantity-1);
+
+        }
+    }
+    // const handleFocus = () => {
+    //     // Store the original quantity when the input field gains focus
+    //     setOriginalQuantity(productQuantity);
+    // };
+
+    const handleBlur = () => {
+        // Reset the quantity to the original value if the entered value is invalid
+        if (productQuantity < 1 || productQuantity > product.stock) {
+            setProductQuantity(1);
+        }
+    };
+
     return (
     <>
         <Navbar />
@@ -92,6 +134,9 @@ export default function ProductDetails() {
             <div className={`alert alert-${alertColor}`} role="alert">
             {alertMessage}
             </div>
+        )}
+        {showAlert && (
+                <Alert severity="error">Please enter a quantity between 1 and {product.stock}.</Alert>
         )}
         {product && 
             <div className="container py-3">
@@ -105,7 +150,29 @@ export default function ProductDetails() {
 
                         <p className="card-text display-6 fw-bold" style={{fontSize: "20px"}}>{product.price}$</p>
                         <p style={{marginTop: "5%"}}><b>Quantity</b></p>
-                        <input onChange={e=>{setProductQuantity(e.target.value)}} type="number" id="quantity" name="quantity" placeholder={`Stock Available: ${product.stock}`} min="1" max={product.stock} />
+                        {/* <div class="input-group-prepend mr-3">
+                            <button onClick={decreaseProductQuantity}class="btn btn-dark btn-sm" id="minus-btn"><i class="fa fa-minus"></i></button>
+                        </div>
+                        <input type="text" class="form-control form-control-sm" value={productQuantity} placeholder={`Stock Available: ${product.stock}`} min="1" max={`${product.stock}`} disabled/>
+                        <div class="input-group-append ml-3">
+                            <button onClick={addProductQuantity} class="btn btn-dark btn-sm" id="plus-btn"><i class="fa fa-plus"></i></button>
+                        </div> */}
+                        <TextField
+                            id="outlined-number"
+                            label="Number"
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            min="1"
+                            max={product.stock}
+                            onChange={handleQuantityChange}
+                            // onFocus={handleFocus}
+                            onBlur={handleBlur}
+                            
+                        />
+                        {/* <input onChange={e=>{setProductQuantity(e.target.value)}} type="number" id="quantity" name="quantity" placeholder={`Stock Available: ${product.stock}`} min="1" max={product.stock} /> */}
+                        
                         <button onClick={addToCart} type="button" className="btn btn-sm btn-outline-dark mx-2">Add to Cart</button>
 
                     </div>
