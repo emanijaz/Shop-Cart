783cbb1ee678dc4488ab0cecb7a2ca939851785b
emanFriendTales <eman@friendtales.com>
2024-03-12 15:53:59 +0500

    changed ui/ux

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -5,6 +5,9 @@ import Footer from './Footer';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import '../styles.css'; 
 
 export default function Homepage() {
     const [products, setProducts] = useState([]);
@@ -17,7 +20,6 @@ export default function Homepage() {
         '@media (min-width: 768px)': {
             height: '100vh !important'
         },
-        background: "#eee"
             
     }
     
@@ -58,7 +60,7 @@ export default function Homepage() {
                 return (
                 <div key={product._id} className='row'>
                     <div className='col-md-3 mt-3 mb-1'>
-                        <div className="card h-100 text-center" style={{ width: "18rem" }}>
+                        <div className="card h-100 text-center border-0 card-hover" style={{ width: "18rem" }} >
                             <img className="card-img-top mt-3" src={`/assets/${product.images[0].url}`} alt="Android 1" style={{ height: "250px" }} />
                             <div className="card-body">
                             <p className="card-text" style={{ fontSize: "16px" }}>{product.name}</p>
@@ -66,7 +68,7 @@ export default function Homepage() {
                             <p className="card-text" style={{ fontSize: "18px" }}><i className="fa fa-solid fa-tags me-1"></i>{product.price}</p>
                             
                             <Link to={`/product/${product._id}`} key={product._id} className='col-md-3 mt-3 mb-1'>
-                                <button type="button" className="btn btn-outline-dark mx-2">Buy</button>
+                                <Button size="small" style={{ color: 'black', borderColor: 'black' }} variant="outlined">Add to Cart</Button>
                             </Link>
                             </div>
                         </div>
@@ -75,13 +77,13 @@ export default function Homepage() {
                     {/* Render the remaining 3 products in the same row */}
                     {products.slice(index + 1, index + 4).map((nextProduct) => (
                     <div key={nextProduct._id} className='col-md-3 mt-3 mb-1'>
-                        <div className="card h-100 text-center" style={{ width: "18rem" }}>
+                        <div className="card h-100 text-center border-0" style={{ width: "18rem" }}>
                         <img className="card-img-top mt-3" src={`/assets/${nextProduct.images[0].url}`} alt="Android 1" style={{ height: "250px" }} />
                         <div className="card-body">
                             <p className="card-text" style={{ fontSize: "16px" }}>{nextProduct.name}</p>
                             <p className="card-text" style={{ fontSize: "18px" }}><i className="fa fa-solid fa-tags me-1"></i>{nextProduct.price}</p>
                             <Link to={`/product/${nextProduct._id}`} key={nextProduct._id} className='col-md-3 mt-3 mb-1'>
-                                <button type="button" className="btn btn-outline-dark mx-2">Buy</button>
+                                <Button size="small" style={{ color: 'black', borderColor: 'black' }} variant="outlined">Add to Cart</Button>
                             </Link>
                         </div>
                         </div>
@@ -102,8 +104,9 @@ export default function Homepage() {
                 
                     
                         <Navbar />
-                        <Slider />
+                        
                         <div id="products">
+                            <Slider />
                             { products ? 
                             <div className="container py-3" >
                                 <h3 className='d-md-flex justify-content-center mt-5 mb-3'>All Products </h3>
@@ -116,8 +119,25 @@ export default function Homepage() {
                             
                             }
                         </div>
+                        {products ? null : (
+        
+                            <div style={{
+                                position: 'fixed',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                height: '100%',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                backgroundColor: 'white',
+                                zIndex: 9999,
+                                }}>
+                                <CircularProgress/>
+                            </div>
+                        )}
                         <div id="contacts">
-                        <Footer /> 
+                            <Footer /> 
                         </div>
                     
             </div>
--- frontend/src/components/ProductDetails.jsx
+++ frontend/src/components/ProductDetails.jsx
@@ -10,6 +10,7 @@ import TextField from '@mui/material/TextField';
 import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import CircularProgress from '@mui/material/CircularProgress';
 import Footer from './Footer';
 
 
@@ -132,123 +133,146 @@ export default function ProductDetails() {
         {showAlert && (
                 <Alert severity={alertSeverity}>{alertMessage}</Alert>    
         )}
-        {product && 
-            <div className="container py-3">
-                <div className='row'>
-                    <div className='col-md-6 mt-5 mb-1'>
-                        <img src={`/assets/${product.images[0].url}`} alt="" style={{height: "500px", width: "500px"}} />
-                    </div>
-                    <div className='col-md-6 mt-5 mb-1'>
-                        <h3>{product.name}</h3><hr/>
-                        <p style={{marginTop: "5%"}}>{product.description}</p>
+        <div className="content-container">
+            {product ? (
+                <div className="container py-3">
+                    
+                            <div className='row'>
+                                <div className='col-md-6 mt-5 mb-1'>
+                                    <img src={`/assets/${product.images[0].url}`} alt="" style={{height: "500px", width: "500px"}} />
+                                </div>
+                                <div className='col-md-6 mt-5 mb-1'>
+                                    <h3>{product.name}</h3><hr/>
+                                    <p style={{marginTop: "5%"}}>{product.description}</p>
 
-                        <p className="card-text display-6 fw-bold" style={{fontSize: "20px"}}>{product.price}$</p>
-                        <p style={{marginTop: "5%"}}><b>Quantity</b></p>
-                        <TextField
-                            id="outlined-number"
-                            label="Number"
-                            type="number"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            min="1"
-                            max={product.stock}
-                            onChange={handleQuantityChange}
-                            placeholder={`Stock Available: ${product.stock}`}
-                            size="small"
-                        />
-                        <div style={{marginTop: "2%"}}>
-                        <Button onClick={addToCart} style={{backgroundColor: "black"}} variant="contained" endIcon={<ShoppingCartIcon />}>
-                            Add To Cart
-                        </Button>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div className='row'>
-                            {product.numOfReviews > 0 ? 
-                            <div class="card mt-3 mb-1 border border-bottom" style={{height: "150px", width: "500px"}}>
-                                <div class="card-body">
-                                    <h5 class="card-title">Rating</h5>
-                                    <p class="card-text">
-                                        <Typography component="legend">Based on {product.numOfReviews} reviews</Typography>
-                                        <Rating name="read-only" value={product.numOfReviews} readOnly />
-                                    </p>
+                                    <p className="card-text display-6 fw-bold" style={{fontSize: "20px"}}>{product.price}$</p>
+                                    <p style={{marginTop: "5%"}}><b>Quantity</b></p>
+                                    <TextField
+                                        id="outlined-number"
+                                        label="Number"
+                                        type="number"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        min="1"
+                                        max={product.stock}
+                                        onChange={handleQuantityChange}
+                                        placeholder={`Stock Available: ${product.stock}`}
+                                        size="small"
+                                    />
+                                    <div style={{marginTop: "2%"}}>
+                                    <Button onClick={addToCart} style={{backgroundColor: "black"}} variant="contained" endIcon={<ShoppingCartIcon />}>
+                                        Add To Cart
+                                    </Button>
+                                    </div>
                                 </div>
                             </div>
-                            : "No Reviews"
-                            }
-                        </div>
-                        <div className='row'>
-                            <div class="card mt-3 mb-1 border border-bottom" style={{height: "350px", width: "500px"}}>
-                                <div class="card-body">
-                                <h5 class="card-title">Add Review</h5>
-                                    <p class="card-text">
-                                        <div className='mt-3'>
-                                            <form >
-                                                <div className="form-group my-3">
-                                                    <label className='mb-1' for="title">Title</label>
-                                                    <input type="text" class="form-control" id="title"  placeholder="Enter title"  onChange={(e) => setReviewTitle(e.target.value)}/>
-                                                </div>
-                                                <div className="form-group my-3">
-                                                    <label for="comment">Review</label>
-                                                    <textarea type="text" class="form-control" id="comment" placeholder="Write Review" onChange={(e) => setReviewComment(e.target.value)}/>
-                                                </div>
-                                                <div className="form-group">
-                                                        <Rating size='small'
-                                                        name="simple-controlled"
-                                                        value={reviewRating}
-                                                        onChange={(event, newValue) => {
-                                                        setReviewRating(newValue);
-                                                        }}
-                                                    />
-                                                </div>
-                                                <div>
-                                                    <button onClick={handleSubmit} className="btn btn-sm btn-outline-dark mt-2">Publish Review</button>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    
-                                    </p>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                    {
-                        product.reviews.map((review)=>{
-                            return (
-                            <div key={review.title} class="card mt-3 mb-1 border border-bottom">
-                                <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
                                     <div className='row'>
-                                        <div className='col-md-2 d-md-flex justify-content-center mt-md-4'>
-                                            <Avatar src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg" size="40" round={true} />
-                                        </div>
-                                        <div className='col-md-8'>
-                                        <div className='row'>
-                                            <div className='col-md-8'>
-                                                <h5 class="card-title">{review.title}</h5>
-                                            </div>
-                                            <div className='col-md-4 mb-3'>
+                                        {product.numOfReviews > 0 ? 
+                                        <div class="card mt-3 mb-1 border border-bottom" style={{height: "150px", width: "500px"}}>
+                                            <div class="card-body">
+                                                <h5 class="card-title">Rating</h5>
                                                 <p class="card-text">
-                                                    <Rating name="read-only" value={review.rating} size="small" readOnly />
+                                                    <Typography component="legend">Based on {product.numOfReviews} reviews</Typography>
+                                                    <Rating name="read-only" value={product.numOfReviews} readOnly />
                                                 </p>
                                             </div>
                                         </div>
-                                        <p>{review.comment}</p>
+                                        : "No Reviews"
+                                        }
+                                    </div>
+                                    <div className='row'>
+                                        <div class="card mt-3 mb-1 border border-bottom" style={{height: "350px", width: "500px"}}>
+                                            <div class="card-body">
+                                            <h5 class="card-title">Add Review</h5>
+                                                <p class="card-text">
+                                                    <div className='mt-3'>
+                                                        <form >
+                                                            <div className="form-group my-3">
+                                                                <label className='mb-1' for="title">Title</label>
+                                                                <input type="text" class="form-control" id="title"  placeholder="Enter title"  onChange={(e) => setReviewTitle(e.target.value)}/>
+                                                            </div>
+                                                            <div className="form-group my-3">
+                                                                <label for="comment">Review</label>
+                                                                <textarea type="text" class="form-control" id="comment" placeholder="Write Review" onChange={(e) => setReviewComment(e.target.value)}/>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                    <Rating size='small'
+                                                                    name="simple-controlled"
+                                                                    value={reviewRating}
+                                                                    onChange={(event, newValue) => {
+                                                                    setReviewRating(newValue);
+                                                                    }}
+                                                                />
+                                                            </div>
+                                                            <div>
+                                                                <button onClick={handleSubmit} className="btn btn-sm btn-outline-dark mt-2">Publish Review</button>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                
+                                                </p>
+                                                
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>)
-                        })
-                    }
-                    </div>
-                </div>
-            </div>
+                                <div class="col-md-6">
+                                {
+                                    product.reviews.map((review)=>{
+                                        return (
+                                        <div key={review.title} class="card mt-3 mb-1 border border-bottom">
+                                            <div class="card-body">
+                                                <div className='row'>
+                                                    <div className='col-md-2 d-md-flex justify-content-center mt-md-4'>
+                                                        <Avatar src="https://raw.githubusercontent.com/twbs/icons/main/icons/person-fill.svg" size="40" round={true} />
+                                                    </div>
+                                                    <div className='col-md-8'>
+                                                    <div className='row'>
+                                                        <div className='col-md-8'>
+                                                            <h5 class="card-title">{review.title}</h5>
+                                                        </div>
+                                                        <div className='col-md-4 mb-3'>
+                                                            <p class="card-text">
+                                                                <Rating name="read-only" value={review.rating} size="small" readOnly />
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                    <p>{review.comment}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>)
+                                    })
+                                }
+                                </div>
+                            </div>
 
-        }
+                </div>
+                ): null 
+            }
+        </div>
+        {product ? null : (
+        
+            <div style={{
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'white',
+                zIndex: 9999,
+                }}>
+                <CircularProgress/>
+            </div>
+        )}
+        
+        
         <Footer /> 
 
     </>
