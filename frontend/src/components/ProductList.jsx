58cc43f12023e50eecd209aff3ede6a6406ac740
emanFriendTales <eman@friendtales.com>
2024-03-21 23:14:14 +0500

    added price filter

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -28,85 +28,67 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function ProductList() {
-    const [selectedCategory, setSelectedCategory] = useState('Mobile');
+    const [selectedCategory, setSelectedCategory] = useState({'text': 'Mobile'});
     const [selectedPrices, setSelectedPrices] = useState([]);
     const [products, setProducts] = useState([]);
 
     const handleCategoryItemClick = (item) => {
-        console.log(item)
         setSelectedCategory(item);
     };
 
-    // const handlePriceItemClick = (item) => {
-    //     const selectedIndex = selectedPrices.indexOf(item.range);
-    //     let newSelected = [];
-
-    //     if (selectedIndex === -1) {
-    //         newSelected = newSelected.concat(selectedPrices, item.range);
-    //         console.log(newSelected)
-    //     } else if (selectedIndex === 0) {
-    //         newSelected = newSelected.concat(selectedPrices.slice(1));
-    //     } else if (selectedIndex === selectedPrices.length - 1) { // removing last element
-    //         newSelected = newSelected.concat(selectedPrices.slice(0, -1));
-    //     } else if (selectedIndex > 0) {   // removing any element other than last and first
-    //         newSelected = newSelected.concat(
-    //             selectedPrices.slice(0, selectedIndex),
-    //             selectedPrices.slice(selectedIndex + 1)
-    //         );
-    //     }
-
-    //     setSelectedPrices(newSelected);
-    // };
     const handlePriceItemClick = (item) => {
-        console.log(item)
-        const selectedIndex = selectedPrices.findIndex(priceRange => JSON.stringify(priceRange) === JSON.stringify(item.range));
+        const isSelected = selectedPrices.some(priceRange => 
+            JSON.stringify(priceRange) === JSON.stringify(item.range)
+        );
+        
         let newSelected = [];
-    
-        if (selectedIndex === -1) {
+        if (!isSelected) {
             // If the range is not already selected, add it to the selectedPrices array
             newSelected = [...selectedPrices, item.range];
         } else {
             // If the range is already selected, remove it from the selectedPrices array
-            newSelected = selectedPrices.filter((range, index) => index !== selectedIndex);
+            newSelected = selectedPrices.filter(priceRange => {
+                return JSON.stringify(priceRange) !== JSON.stringify(item.range)
+            });   
+        
         }
-    
         setSelectedPrices(newSelected);
     };
 
 
     function FormRow() {   
         return products.map((product, index) => {
-            if (index % 3 === 0) {
+            if (index % 4 === 0) {
                 return (
-                <React.Fragment>
-                    <Grid item xs={4} sx={{ border: 'none' }}>
+                <React.Fragment className="border-0">
+                    <Grid item xs={4} className='border-0'>
                             <Item>
-                                        <div className="card h-100 text-center border-0" style={{ width: "18rem" }}>
+                                <Link to={`/product/${product._id}`} key={product._id} className='col-md-3 mt-3 mb-1' style={{textDecoration: "none"}}>
+                                    <div className="card h-100 text-center border-0" style={{ width: "18rem" }}>
                                         <img className="card-img-top mt-3" src={`/assets/${product.images[0].url}`} alt="Android 1" style={{ height: "250px" }} />
                                         <div className="card-body">
                                             <p className="card-text" style={{ fontSize: "16px" }}>{product.name}</p>
                                             <p className="card-text" style={{ fontSize: "18px" }}><i className="fa fa-solid fa-tags me-1"></i>{product.price}</p>
-                                            <Link to={`/product/${product._id}`} key={product._id} className='col-md-3 mt-3 mb-1'>
-                                                <Button size="small" style={{ color: 'black', borderColor: 'black' }} variant="outlined">Add to Cart</Button>
-                                            </Link>
+                                            
                                         </div>  
-                                        </div>
+                                    </div>
+                                </Link>
                             </Item>
                     </Grid>
     
-                    {products.slice(index + 1, index + 3).map((nextProduct) => (
-                        <Grid item xs={4} sx={{ border: 'none' }}>
+                    {products.slice(index + 1, index + 4).map((nextProduct) => (
+                        <Grid id={nextProduct._id} item xs={4} className='border-0'>
                             <Item>
+                                    <Link to={`/product/${nextProduct._id}`} key={product._id} className='col-md-3 mt-3 mb-1' style={{textDecoration: "none"}}>
                                         <div className="card h-100 text-center border-0" style={{ width: "18rem" }}>
                                         <img className="card-img-top mt-3" src={`/assets/${nextProduct.images[0].url}`} alt="Android 1" style={{ height: "250px" }} />
                                         <div className="card-body">
                                             <p className="card-text" style={{ fontSize: "16px" }}>{nextProduct.name}</p>
                                             <p className="card-text" style={{ fontSize: "18px" }}><i className="fa fa-solid fa-tags me-1"></i>{nextProduct.price}</p>
-                                            <Link to={`/product/${nextProduct._id}`} key={product._id} className='col-md-3 mt-3 mb-1'>
-                                                <Button size="small" style={{ color: 'black', borderColor: 'black' }} variant="outlined">Add to Cart</Button>
-                                            </Link>
+                                            
                                         </div>
                                         </div>
+                                    </Link>
                             </Item>
                         </Grid>
                     ))}
@@ -128,18 +110,13 @@ export default function ProductList() {
                 });
                 const data = await response.json();
                 if(data.success){
-                    console.log(data.products)
                     const filteredProducts = data.products.filter(product => {
-                        console.log(product.category)
-                        console.log(selectedCategory.text)
-                        console.log(product.category === selectedCategory.text)
                         const categoryMatch = selectedCategory ? product.category === selectedCategory.text : true;
-                        
-                        // const priceMatch = selectedPrices.length === 0 || selectedPrices.includes(product.price);
-                        // console.log(priceMatch)
-                        return categoryMatch;
+                        const priceMatch = selectedPrices.length === 0 || selectedPrices.some(priceRange => {
+                            return product.price >= priceRange[0] && product.price <= priceRange[1];
+                        });
+                        return categoryMatch && priceMatch;
                     });
-                    console.log('filtered prods: ', filteredProducts)
                     setProducts(filteredProducts);
                 }
             }
@@ -156,8 +133,8 @@ export default function ProductList() {
             <Navbar />
             <Container maxWidth="30">
                 <h3 className='d-md-flex justify-content-center mt-5 mb-3'>Product List </h3>
-                <Box sx={{  height: '100vh', padding: '2%', flexGrow: 1}}>
-                    <Grid container spacing={3} columns={16}>
+                <Box sx={{  height: '100vh', paddingTop: '2%', px: "5%", flexGrow: 1}}>
+                    <Grid container spacing={6} columns={12}>
                         <Grid item xs={3}>
                             <Item>
                                 <Typography variant="h6">Categories</Typography>
@@ -212,11 +189,11 @@ export default function ProductList() {
                                     <ListItemButton
                                         key={item.id}
                                         onClick={() => handlePriceItemClick(item)}
-                                        style={{ color: selectedPrices.includes(item.id) ? 'black' : 'grey',  }}
-                                    >
+                                        style={{ color: selectedPrices.some(priceRange => JSON.stringify(priceRange) === JSON.stringify(item.range)) ? 'black' : 'grey',  }}>
                                         <ListItemIcon>
                                             <Checkbox
-                                                checked={selectedPrices.includes(item.id)}
+                                                checked={selectedPrices.some(priceRange => 
+                                                    JSON.stringify(priceRange) === JSON.stringify(item.range))}
                                                 onChange={() => handlePriceItemClick(item)}
                                                 size="small"
                                             />
@@ -224,7 +201,8 @@ export default function ProductList() {
                                         <ListItemText>
                                             <Typography
                                                 variant="body2"
-                                                style={{color: selectedPrices.includes(item.id) ? 'black' : 'grey', }}
+                                                style={{color: selectedPrices.some(priceRange => 
+                                                    JSON.stringify(priceRange) === JSON.stringify(item.range)) ? 'black' : 'grey', }}
                                             >
                                                 ${item.range[0]} - ${item.range[1]} 
                                             </Typography>
@@ -236,7 +214,7 @@ export default function ProductList() {
                         </Grid>
                         <Grid item xs={9}>
                             {products ? (
-                                <Grid container sx={{ border: 'none' }}>
+                                <Grid container>
                                     <FormRow />
                                 </Grid>
                             ) : <p>No products found</p>}
