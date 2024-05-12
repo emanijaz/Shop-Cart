2145642eac1b2f986338180286aa366adddbada7
emanFriendTales <eman@friendtales.com>
2024-05-12 23:12:16 +0500

    fixing settings issues

--- frontend/src/components/Account.jsx
+++ frontend/src/components/Account.jsx
@@ -13,6 +13,8 @@ import { styled } from '@mui/system';
 import clsx from 'clsx';
 import EditIcon from '@mui/icons-material/Edit'
 import Avatar from '@mui/material/Avatar';
+import Alert from '@mui/material/Alert';
+
 
 const StyledInput = styled(Input)(
     ({ theme }) => `
@@ -101,30 +103,68 @@ const grey = {
 
 export default function Account() {
     const [userData, setUserData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    gender: ''});
+        firstName: { value: '', isValid: true },
+        lastName: { value: '', isValid: true },
+        email: { value: '', isValid: true },
+        phone: { value: '', isValid: true },
+        gender: { value: '', isValid: true }
+    });
+    const [alertMessage, setAlertMessage] = useState('');
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertSeverity, setAlertSeverity] = useState('');
 
     const handleInputChange = (event) => {
-        console.log('in input change')
-        const { name, value } = event.target;
-        console.log(value)
-        setUserData({ ...userData, [name]: value });
+        let { name, value } = event.target;
+        if(name === 'phone'){
+            value = value.replace(/\D/g, '');
+        }
+        value = event.target.type === 'select-one' ? event.target.selectedOptions[0].text : value;
+        // setUserData({ ...userData, [name]: value });
+        setUserData({ ...userData, [name]: { value, isValid: validateField(name, value) } });
+    };
+    const validateField = (name, value) => {
+        if (name === 'email') {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(value);
+        }
+        if(name == 'phone'){
+            const phoneRegex = /^\d{0,10}$/;
+            return phoneRegex.test(value);
+        }
+        return true;
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        console.log('user data in submit: ', userData)
+        const isAnyValidationFailed = Object.values(userData).some(field => !field.isValid);
+
+        if (isAnyValidationFailed) {
+            // If any validation failed, prevent form submission
+            console.log('validation failed')
+            return;
+        }
         try {
+            const requestBody = {};
+            Object.entries(userData).forEach(([fieldName, fieldValue]) => {
+                requestBody[fieldName] = fieldValue.value;
+            });
+            console.log('req body ;', requestBody)
             const accessToken = localStorage.getItem('accessToken');
-            const response = await axios.put(`http://localhost:5000/users/update/${userData._id}`, userData,{
+            const response = await axios.put(`http://localhost:5000/users/update/${requestBody._id}`, requestBody,{
                 headers: {
                     Authorization: `Bearer ${accessToken}`
                 },
                 withCredentials: true,
             });
-            console.log('User updated successfully:', response.data);
+            setAlertMessage('Settings Updated');
+            setAlertSeverity('success');
+            setShowAlert(true);
+            setTimeout(() => {
+                setAlertMessage('');
+                setShowAlert(false);
+                setAlertSeverity('');
+            }, 2000);
         } catch (error) {
             console.error('Error updating user:', error);
         }
@@ -139,7 +179,6 @@ export default function Account() {
                 },
                 withCredentials: true,
             });
-            console.log('response: ', response)
             return response.data;
         } catch (error) {
             console.error('Error fetching user data:', error);
@@ -149,8 +188,17 @@ export default function Account() {
     useEffect(() => {
         const getUserData = async () => {
             const userData = await fetchUserData();
+            console.log('user data: ', userData)
             if (userData) {
-                setUserData(userData);
+                setUserData(prevUserData => ({
+                    ...prevUserData,
+                    _id: {value: userData._id},
+                    firstName: { value: userData.firstName || '', isValid: true },
+                    lastName: { value: userData.lastName || '', isValid: true },
+                    email: { value: userData.email || '', isValid: validateField('email', userData.email) },
+                    phone: { value: userData.phone || '', isValid: validateField('phone', userData.phone) },
+                    gender: { value: userData.gender || '', isValid: true }
+                }));
             }
         };
 
@@ -160,6 +208,9 @@ export default function Account() {
     return (
             <>
                 <Navbar />
+                {showAlert && (
+                    <Alert severity={alertSeverity}>{alertMessage}</Alert>    
+                )}
                 <Container maxWidth="30">
                     <Box sx={{  height: '100vh', paddingTop: '5%', px: "10%", flexGrow: 1}}>
                         <Grid container spacing={1} columns={12}>
@@ -207,37 +258,35 @@ export default function Account() {
                                         <div className="col-md-6">
                                             <div className="form-group">
                                                 <label htmlFor="first_name">First Name</label>
-                                                <input type="text" className="form-control" id="first_name" name="firstName" placeholder="First Name" value={userData?.firstName || ''} onChange={handleInputChange}/>
+                                                <input type="text" className="form-control" id="first_name" name="firstName" placeholder="First Name" value={userData?.firstName?.value || ''} onChange={handleInputChange}/>
                                             </div>
                                         </div>
                                         <div className="col-md-6">
                                             <div className="form-group">
                                                 <label htmlFor="last_name">Last Name</label>
-                                                <input type="text" className="form-control" id="last_name" name="lastName" placeholder="Last Name" value={userData?.lastName || ''} onChange={handleInputChange}/>
+                                                <input type="text" className="form-control" id="last_name" name="lastName" placeholder="Last Name" value={userData?.lastName?.value || ''} onChange={handleInputChange}/>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="form-group mb-3">
                                         <label for="email">Email</label>
-                                        <input type="email" class="form-control" id="email" name="email" placeholder="Email" value={userData?.email || ''} onChange={handleInputChange}/>
+                                        <input type="email" class="form-control" id="email" name="email" placeholder="Email" value={userData?.email?.value || ''} onChange={handleInputChange}/>
                                     </div>
                                     <div class="form-group mb-3">
                                         <label for="phone">Phone</label>
-                                        <input type="phone" class="form-control" id="phone" name="phone" placeholder="Phone" value={userData?.phone || ''} onChange={handleInputChange}/>
+                                        <input type="tel" class="form-control" id="phone" name="phone" placeholder="Phone" value={userData?.phone?.value || '0000000'} onChange={handleInputChange}/>
                                     </div>
                                     <div class="form-row mb-3">
-                                        
                                         <div class="form-group col-md-4">
                                             <label for="inputGender">Gender</label>
-                                            <select id="inputGender" class="form-control" name="gender" onChange={handleInputChange}>
-                                                <option selected>Choose...</option>
+                                            <select id="inputGender" class="form-control" name="gender" onChange={handleInputChange} value={userData.gender.value}>
+                                                <option >Choose...</option>
                                                 <option>Female</option>
                                                 <option>Male</option>
                                                 <option>Other</option>
 
                                             </select>
                                         </div>
-                                        
                                     </div>
                                     <button type="submit" className="btn btn-dark btn-block"  onClick={handleSubmit}
                                         style={{ 
